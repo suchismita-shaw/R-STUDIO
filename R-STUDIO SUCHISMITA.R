34f5360d7f39f93bd631de187ef513d1db87e81a
Suchismita Shaw <suchismita10.shaw@gmail.com>
2024-08-12 23:15:12 +0530

    Add files via upload

---
+++ R-STUDIO SUCHISMITA.R
@@ -0,0 +1,62 @@
+>"rini"
+stud_name<-'rini'
+'<suchismita'
+result<- 5 + 3
+print(result)
+typeof(stud_name)
+
+result<-"(10 - 4)"
+print(result)
+#printing
+print("name")
+'video'
+
+a <- "suchismita"
+a_b <- "suchismita"
+ab <- "rini"
+ab_2 <- "rini"
+
+#Arithmetric operators
+a=5
+b=2
+
+c<-a+b
+print(c)
+
+c<-a*b
+print(c)
+
+c<-a/b
+print(c)
+
+c<-a%%b
+print(c)
+
+c<-a%/%b
+print(c)
+
+c<-a^b
+print(c)
+
+a<-5
+b<-6
+
+a==b
+a!=b
+a>=b
+a<=b
+a<b
+a>b
+
+a!=b && a<b
+a!=b || a>b
+!(a==b)
+a!=b && a>b
+a==b || a>b
+
+#single '&' element wise logical operator
+c(TRUE,FALSE,FALSE) & c(FALSE,FALSE,FALSE)
+c(TRUE,FALSE,FALSE) & c(TRUE,FALSE,FALSE)
+
+c(TRUE,FALSE,FALSE) || c(TRUE,FALSE,FALSE)
+
